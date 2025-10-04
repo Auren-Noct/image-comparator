cbd1ef96fef897aabcfe49d803edd0c776df3e8e
Auren-Noct <LoneWolf4991@gmail.com>
2025-10-03 21:13:31 -0300

    feat: PWA integration and UI enhancements
    - Added PWA support
    - Updated icons in index.html
    - Added theme-color meta tag

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,72 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "screenshot-desktop.png",
+        "screenshot-mobile.png",
+      ],
+      manifest: {
+        name: "Image Comparator",
+        short_name: "ImgComp",
+        description:
+          "Compara dos imágenes y visualiza las diferencias y similitudes.",
+        theme_color: "#111827",
+        background_color: "#ffffff",
+        lang: "es",
+        scope: "/image-comparator/",
+        start_url: "/image-comparator/",
+        display: "standalone",
+        id: "/image-comparator/",
+        orientation: "any",
+        categories: ["utilities", "photo", "graphics"],
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        screenshots: [
+          {
+            src: "screenshot-desktop.png",
+            sizes: "1280x720",
+            type: "image/png",
+            form_factor: "wide",
+            label: "Vista de escritorio de la aplicación",
+          },
+          {
+            src: "screenshot-mobile.png",
+            sizes: "540x720",
+            type: "image/png",
+            form_factor: "narrow",
+            label: "Vista móvil de la aplicación",
+          },
+        ],
+      },
+    }),
+  ],
   base: "https://Auren-Noct.github.io/image-comparator",
   server: {
     host: true,
